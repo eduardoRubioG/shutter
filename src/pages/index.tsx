22a87744db0174069d7ef5ab25fb0267ec7cdf47
Eduardo <eduardo.rubio.jr85@gmail.com>
2022-12-22 11:52:53 -0500

    Adding a netlify form marker to see if we can register forms inside the canvas element

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-import Nav from "../components/Nav/Nav"
+import GhostForm, {
+  GhostFormElements,
+  GhostFormTypes,
+} from "../components/GhostForm/GhostForm";
+import Nav from "../components/Nav/Nav";
 import SplashCanvasWrapper from "../components/Three/SplashCanvasWrapper/SplashCanvasWrapper";
 
 interface AppProps {}
@@ -7,9 +11,29 @@ interface AppProps {}
 const App: React.FunctionComponent<AppProps> = () => {
   return (
     <main>
-      <Nav /> 
+      <Nav />
       <SplashCanvasWrapper />
       {/* Footer rolled into the scroll composition  */}
+      <GhostForm
+        formName="Landing Page Contact"
+        fields={[
+          {
+            name: "name",
+            type: GhostFormTypes.TEXT,
+            elementType: GhostFormElements.INPUT,
+          },
+          {
+            name: "email",
+            type: GhostFormTypes.EMAIL,
+            elementType: GhostFormElements.INPUT,
+          },
+          {
+            name: "message",
+            type: GhostFormTypes.TEXT,
+            elementType: GhostFormElements.INPUT,
+          },
+        ]}
+      />
     </main>
   );
 };
